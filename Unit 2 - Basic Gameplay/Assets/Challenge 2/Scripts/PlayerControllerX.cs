9ae415a82d482646612cd1fb04878b2641608cb8
Daniel Herrera Cardona <134321751+DanielHerreraC97@users.noreply.github.com>
2023-07-19 09:27:23 -0500

    Unit 2 Project

---
+++ Unit 2 - Basic Gameplay/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float spawnInterval = 2.0f;  // Time interval between dog spawns
+    private float lastSpawnTime = 0.0f;  // Time of the last dog spawn
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check if enough time has passed since the last spawn
+        if (Time.time - lastSpawnTime >= spawnInterval)
+        {
+            // On spacebar press, send dog and update the last spawn time
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastSpawnTime = Time.time;
+            }
+        }
+    }
+}
